1b506ec970ea8d1d9b990c3c3549bdcd5ad62222
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-15 17:00:03 +0100

    IbAction

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Payer.swift
+//  app-swoosh
+//
+//  Created by Artur Zarzecki on 15/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
